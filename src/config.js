9137b76f1470fceef75360db292a7af1036ca701
Jon Kimmel <jonkimmel16@gmail.com>
2020-04-20 23:57:21 -0700

    token update

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ export default {
   API_ENDPOINT: "https://boiling-dawn-55684.herokuapp.com",
   //API_ENDPOINT: "http://localhost:8000",
   FRONT_WEB: "*",
-  TOKEN_KEY: "change-this-secret",
+  TOKEN_KEY: process.env.REACT_APP_API_KEY,
 };
