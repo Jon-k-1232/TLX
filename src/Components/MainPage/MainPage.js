779dd92c6676e83c476e1a1dfde2bed975f877c7
Jon Kimmel <jonkimmel16@gmail.com>
2020-04-22 18:30:45 -0700

    start page css

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./MainPage.css";
+import { Link } from "react-router-dom";
+import billing from "../Images/billing.png";
+import communication from "../Images/communication.png";
+import handShake from "../Images/handShake.png";
 
 /*
  Main Welcome Page.
@@ -8,8 +12,27 @@ export default class MainPage extends React.Component {
   render() {
     return (
       <main className="mainPage">
-        <div>
-          <h1>THIS IS THE MAIN SCREEN</h1>
+        <h2>Tenant Landlord Exchange</h2>
+        <div className="homeCall">
+          <Link to="/Sign-in">
+            <div>
+              <img src={billing} alt="billing icon" />
+              <h3>Better Statements</h3>
+            </div>
+          </Link>
+
+          <Link to="/Sign-in">
+            <div>
+              <img src={communication} alt="message icon" />
+              <h3>Better Communication</h3>
+            </div>
+          </Link>
+          <Link to="/Sign-in">
+            <div>
+              <img src={handShake} alt="message icon" />
+              <h3>Better Relations</h3>
+            </div>
+          </Link>
         </div>
       </main>
     );
